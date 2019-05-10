3f6ed7cadefd486ddda19edf75bd0446358b36d3
akshat1 <akshat.sh@gmail.com>
2019-05-09 22:31:20 -0700

    Tests for model constructors.

--- test/model.js
+++ test/model.js
@@ -1,7 +1,27 @@
 import assert from 'assert';
-import { Edge, nodesFromEdges } from '../src/model';
+import { Edge, nodesFromEdges, Graph } from '../src/model';
 
 describe('model', function() {
+  describe('Edge', function() {
+    it('should construct an edge', function() {
+      const edge = new Edge('a', 'b');
+      assert.deepEqual(edge, { from: 'a', to: 'b' });
+    });
+  });
+
+  describe('Graph', function() {
+    it('should construct a graph', function() {
+      const edges = [
+        new Edge('a', 'b'),
+        new Edge('a', 'c'),
+        new Edge('d', 'e'),
+        new Edge('b', 'f'),
+      ];
+      const g = new Graph(edges);
+      assert.deepEqual(g.edges, new Set(edges));
+      assert.deepEqual(g.nodes, nodesFromEdges(edges));
+    });
+  })
   describe('nodesFromEdges', function() {
     it('should return a set comprised of nodes from supplied edges', function() {
       const edges = [
