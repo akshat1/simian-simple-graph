a1b9ab4d588ee9e09ebc1bd7109c7dff88e4c54c
akshat1 <akshat.sh@gmail.com>
2019-08-20 22:08:28 -0700

    All new implementation of Graph and Edge utilizing immutable and Object.freeze. This lets us memoize Graph functions. Also, unit tests for the new code.
    Still need to re-implement toDot function.

--- test/Graph.js
+++ test/Graph.js
@@ -1,47 +1,50 @@
+const { Edge } = require('../lib/Edge');
+const { Graph } = require('../lib/Graph');
 const assert = require('assert');
-const { Graph, Edge } = require('../lib');
-
-const { makeEdge } = Edge;
 
 describe('Graph', function() {
   describe('getSubTree', function() {
     it('should supply a simple subTree', function() {
       const graph = new Graph([
-        makeEdge('a', 'aa'),
-        makeEdge('a', 'ab'),
-        makeEdge('a', 'ac'),
-        makeEdge('ab', 'aba'),
-        makeEdge('ab', 'abb'),
-        makeEdge('ab', 'abc'),
-        makeEdge('abb', 'abba'),
-        makeEdge('abb', 'abbb'),
-        makeEdge('abb', 'abbc'),
+        new Edge('a', 'aa'),
+        new Edge('a', 'ab'),
+        new Edge('a', 'ac'),
+        new Edge('ab', 'aba'),
+        new Edge('ab', 'abb'),
+        new Edge('ab', 'abc'),
+        new Edge('abb', 'abba'),
+        new Edge('abb', 'abbb'),
+        new Edge('abb', 'abbc'),
       ]);
 
       const expectedSubTree = new Graph([
-        makeEdge('ab', 'aba'),
-        makeEdge('ab', 'abb'),
-        makeEdge('ab', 'abc'),
-        makeEdge('abb', 'abba'),
-        makeEdge('abb', 'abbb'),
-        makeEdge('abb', 'abbc'),
+        new Edge('ab', 'aba'),
+        new Edge('ab', 'abb'),
+        new Edge('ab', 'abc'),
+        new Edge('abb', 'abba'),
+        new Edge('abb', 'abbb'),
+        new Edge('abb', 'abbc'),
       ]);
 
       const actualSubTree = Graph.getSubTree(graph, 'ab');
 
-      assert.equal(actualSubTree.toString(), expectedSubTree.toString());
+      assert.equal(Graph.toString(actualSubTree), Graph.toString(expectedSubTree));
+    });
+
+    it('should be memoized', function() {
+      assert.ok(typeof Graph.getSubTree._clearCache === 'function');
     });
   });
 
   describe('invert', function() {
     it('should return a new graph with all edges flipped', function() {
       const graph = new Graph([
-        makeEdge('a', 'b'),
-        makeEdge('a', 'c'),
-        makeEdge('a', 'd'),
-        makeEdge('b', 'e'),
-        makeEdge('c', 'f'),
-        makeEdge('d', 'g'),
+        new Edge('a', 'b'),
+        new Edge('a', 'c'),
+        new Edge('a', 'd'),
+        new Edge('b', 'e'),
+        new Edge('c', 'f'),
+        new Edge('d', 'g'),
       ]);
 
       const iGraph = Graph.invert(graph);
@@ -49,30 +52,46 @@ describe('Graph', function() {
 
       assert.equal(Graph.toString(iGraph), expectedString);
     });
+
+    it('should be memoized', function() {
+      assert.ok(typeof Graph.invert._clearCache === 'function');
+    });
   });
 
   describe('Ancestors / Descendents', function() {
     const graph = new Graph([
-      makeEdge('a', 'b'),
-      makeEdge('a', 'c'),
-      makeEdge('a', 'd'),
-      makeEdge('b', 'e'),
-      makeEdge('c', 'f'),
-      makeEdge('d', 'g'),
+      new Edge('a', 'b'),
+      new Edge('a', 'c'),
+      new Edge('a', 'd'),
+      new Edge('b', 'e'),
+      new Edge('c', 'f'),
+      new Edge('d', 'g'),
     ]);
 
     describe('getAncestors', function() {
-      assert.deepEqual(
-        Array.from(Graph.getAncestors(graph, 'g')).sort(),
-        ['a', 'd']
-      );
+      it('should get ancestors', function() {
+        assert.deepEqual(
+          Graph.getAncestors(graph, 'g').toArray().sort(),
+          ['a', 'd']
+        );
+      });
+
+      it('should be memoized', function() {
+        assert.ok(typeof Graph.getAncestors._clearCache === 'function');
+      });
     });
 
     describe('getDescendents', function() {
-      assert.deepEqual(
-        Array.from(Graph.getDescendents(graph, 'a')).sort(),
-        ['b', 'c', 'd', 'e', 'f', 'g'],
-      );
+      it('should get descendents', function() {
+        assert.deepEqual(
+          Array.from(Graph.getDescendents(graph, 'a')).sort(),
+          ['b', 'c', 'd', 'e', 'f', 'g'],
+        );
+      });
+
+      it('should be memoized', function() {
+        assert.ok(typeof Graph.getDescendents._clearCache === 'function');
+      });
     });
   })
 });
